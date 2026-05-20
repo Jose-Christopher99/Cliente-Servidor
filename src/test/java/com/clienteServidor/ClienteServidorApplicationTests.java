82d334a5057d92b8258283a3790fd8e1ebd97199
Christopher Vasquez <jccva1999@gmail.com>
2026-05-20 13:59:52 -0500

    Feat: Se agrego la configuracion de Postgresql con Neon

---
+++ src/test/java/com/clienteServidor/ClienteServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clienteServidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
